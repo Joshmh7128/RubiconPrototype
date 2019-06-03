65d9588f8f57ba7ffb19b2c1f39396c172661f3a
Devin <devin.quinn@uconn.edu>
2019-06-03 16:38:56 -0400

    Added project files

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody playerBody;
+
+    private Vector3 inputVector;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("TrueVertical"), Input.GetAxisRaw("Vertical"));
+        inputVector = transform.TransformDirection(inputVector);
+    }
+
+    private void FixedUpdate()
+    {
+        playerBody.velocity = inputVector * (speed * 50) * Time.deltaTime;
+    }
+}
