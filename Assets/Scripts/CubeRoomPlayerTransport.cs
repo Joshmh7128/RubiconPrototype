88306efe240295e5f401ec99c0c6964b98479b13
Josh <joshmh7128@gmail.com>
2019-06-22 22:28:22 -0400

    Added red and green map designs

---
+++ Assets/Scripts/CubeRoomPlayerTransport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRoomPlayerTransport : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider player)
+    {
+        player.transform.SetParent(transform);  
+    }   
+
+    void OnTriggerExit(Collider player)
+    {
+        player.transform.parent = null;
+    }
+}
