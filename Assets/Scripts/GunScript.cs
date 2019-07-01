4978dec7efeee452c035d01556eb374195f3839e
Devin <devinpquinn4@gmail.com>
2019-07-01 12:24:31 -0400

    Refining splitscreen features
    added health tracking and different materials for different player ballistics, can now fire and reload separately

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -7,7 +7,6 @@ public class GunScript : MonoBehaviour
     public float fireRate = 0.25f;                                        // Number in seconds which controls how often the player can fire
     public float weaponRange = 100f;                                        // Distance in Unity units over which the player can fire
     public Transform gunEnd;                                            // Holds a reference to the gun end object, marking the muzzle location of the gun
-    public bool laserTracking = true;                                     //Whether or not the laser emitter should track to the gun barrel; matter of preference
 
     private Camera fpsCam;                                                // Holds a reference to the first person camera
     private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);    // WaitForSeconds object used by our ShotEffect coroutine, determines time laser line will remain visible
@@ -80,6 +79,10 @@ public class GunScript : MonoBehaviour
                     if (hit.collider.gameObject.CompareTag("Arena"))
                     {
                         Instantiate(burst, hit.point, Quaternion.identity);
+                    }
+                    if(hit.collider.gameObject.CompareTag("Player"))
+                    {
+                        hit.collider.gameObject.GetComponent<HealthTracker>().TakeDamage(1);
                     }
                 }
                 else
---
+++ Assets/Scripts/HealthTracker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTracker : MonoBehaviour
+{
+    public int hp;
+    private int maxHP = 6;
+    public GameObject bar;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = maxHP;
+        bar.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int taken)
+    {
+        hp -= taken;
+        anim.Play("HPbump");
+        if(hp < 0)
+        {
+            hp = 0;
+        }
+        bar.transform.localScale = new Vector3((float)hp / maxHP, 1, 1);
+    }
+}
