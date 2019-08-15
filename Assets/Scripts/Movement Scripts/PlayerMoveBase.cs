794473bf672cac27dd64357acf65342bdee81ad9
Josh <joshmh7128@gmail.com>
2019-08-15 11:16:24 -0400

    Completed controller setup

--- Assets/Scripts/Movement Scripts/PlayerMoveBase.cs
+++ Assets/Scripts/Movement Scripts/PlayerMoveBase.cs
@@ -18,9 +18,27 @@ public class PlayerMoveBase
 	// Update is called once per frame
 	public void Update()
 	{
+        // basic stick based movement
 		inputVector = new Vector3(Input.GetAxis("Joy" + player.playerID + "Axis1"), 0, -Input.GetAxis("Joy" + player.playerID + "Axis2"));
 		inputVector = player.transform.TransformDirection(inputVector);
-	}
+
+        // In order to properly get the bumper buttons you MUST set them manually through the inspector, they are next to each other near the joystick 1 definitions 
+        // up and down movement
+        
+        if (Input.GetButton("Joy" + player.playerID + "Axis5Bump"))
+        {
+            // move up
+            inputVector += new Vector3(0, 1, 0);
+            
+        }
+
+        if (Input.GetButton("Joy" + player.playerID + "Axis4Bump"))
+        {
+            // move down
+            inputVector += new Vector3(0, -1, 0);
+        }
+        
+    }
 
 	public void FixedUpdate()
 	{
