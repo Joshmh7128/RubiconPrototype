e8d2622cb29b0a5f967073ac5a1d2e0fed31478f
Josh <joshmh7128@gmail.com>
2019-09-17 20:11:01 -0400

    Working on Map designs and performance of them

---
+++ Assets/Scripts/DistanceDeactivate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceDeactivate : MonoBehaviour
+{
+    Transform playerOne;
+    Transform playerTwo;
+    bool isEnabled;
+    float renderDistance = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+       playerOne = GameObject.Find("Player1").GetComponent<Transform>();
+       playerTwo = GameObject.Find("Player2").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(playerOne.position, gameObject.transform.position) > renderDistance)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
