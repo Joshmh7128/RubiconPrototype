d29286822edb0eea45850bdc72184be6a8f17f6e
Josh <joshmh7128@gmail.com>
2019-11-05 17:29:31 -0500

    Added preliminary player 1 gyro

---
+++ Assets/Scripts/SimpleMoveTo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMoveTo : MonoBehaviour
+{
+    public Transform addPos;
+    public Vector3 difference;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos;
+        newPos = addPos.position;
+        newPos += difference;
+        transform.position = newPos;
+    }
+}
