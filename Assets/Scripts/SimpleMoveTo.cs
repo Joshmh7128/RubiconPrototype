bda7b060e42f93fe3cab4bb7a61a331d152eb83d
Devin <devinpquinn4@gmail.com>
2019-11-06 17:19:44 -0500

    Gyro Cam tweaks

--- Assets/Scripts/SimpleMoveTo.cs
+++ Assets/Scripts/SimpleMoveTo.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class SimpleMoveTo : MonoBehaviour
 {
-    public Transform addPos;
+    //public Transform addPos;
     public Vector3 difference;
+    public GameObject targetObject;
     // Update is called once per frame
     void Update()
     {
+        /*
         Vector3 newPos;
         newPos = addPos.position;
         newPos += difference;
         transform.position = newPos;
+        */
+
+        Vector3 newPos;
+        newPos = targetObject.transform.position;
+        newPos += difference;
+        transform.position = newPos;
+        transform.LookAt(targetObject.transform);
+
     }
 }
