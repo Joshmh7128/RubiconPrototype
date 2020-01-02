f131eab6e2a1890331f6e86c4eda2e28cd70ff1c
Devin <devinpquinn4@gmail.com>
2020-01-02 00:04:06 -0500

    Added world escape killbox

---
+++ Assets/Scripts/Killbox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killbox : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.GetComponent<InfoTracker>().TakeDamage(1126);
+        }
+    }
+}
