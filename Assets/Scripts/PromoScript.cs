c74efae094bee605d93980027ad88fdc279ff5c9
Devin <devinpquinn4@gmail.com>
2020-01-30 22:40:50 -0500

    Added controllable camera for promotional videos

---
+++ Assets/Scripts/PromoScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromoScript : MonoBehaviour
+{
+    public PlayerController pc;
+    private bool paused = false;
+
+    private void Update()
+    {
+        pc.weaponLocked = true;
+        if (Input.GetKey(KeyCode.P))
+        {
+            paused = !paused;
+        }
+        if(paused)
+        {
+            Time.timeScale = 0;
+            pc.weaponLocked = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
