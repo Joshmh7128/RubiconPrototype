a91ddf5848147d4bc070f24546fee67ca4341774
Devin <devinpquinn4@gmail.com>
2020-05-29 14:09:37 -0400

    Removed mouse and keyboard fallback controls

--- Assets/Main Menu Resources/MainMenuManager.cs
+++ Assets/Main Menu Resources/MainMenuManager.cs
@@ -77,8 +77,8 @@ public class MainMenuManager : MonoBehaviour
         uicam.depth = 0;
 
         // on click listeners
-        //Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         optionsActive = false;
         optionsButton.onClick.AddListener(OptionsMenuToggle);
         creditsButton.onClick.AddListener(ShowCredits);
